1238df763f5aaa52fad82d5e4a825d318eefd1c7
325415776 <325415776@LAB44PC11>
2022-11-09 19:03:06 +0200

    stage0 1.6

---
+++ Stage0/Program5776.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Wellcome5776();
+            Console.ReadKey();
+        }
+
+        static partial void wellcome5876();
+        private static void Wellcome5776()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, Welcome to my first console application", userName);
+        }
+    }
+}
