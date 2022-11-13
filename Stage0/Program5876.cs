9d1350aa9235944fc4336117079c46fbaca2a49c
213865876 <213865876@LAB44PC11>
2022-11-13 14:11:46 +0200

    12

---
+++ Stage0/Program5876.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stage0
+{
+    internal class Program5876
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ll")  ; 
+            Console.ReadKey();
+        }
+
+       
+    }
+}
