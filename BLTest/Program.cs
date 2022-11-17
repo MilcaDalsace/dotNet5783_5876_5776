4ffd9ad29fd237ab5f1ced69218b0ac272503e89
213865876 <213865876@LAB44PC11>
2022-11-17 14:12:09 +0200

    17/11 14:12

---
+++ BLTest/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BLTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+//< Project Sdk = "Microsoft.NET.Sdk" >
+
+//  < PropertyGroup >
+//    < OutputType > Exe </ OutputType >
+//    < TargetFramework > net6.0 </ TargetFramework >
+//    < ImplicitUsings > enable </ ImplicitUsings >
+//    < Nullable > enable </ Nullable >
+//  </ PropertyGroup >
+
+//</ Project >
